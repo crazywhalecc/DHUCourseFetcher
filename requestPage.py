83f7acd719e38507d4cfa88b15fe2b3d81a5c77f
Whale <jesse2061@outlook.com>
2018-09-27 16:39:48 +0800

    fix some bug

--- requestPage.py
+++ requestPage.py
@@ -1,6 +1,7 @@
 import requests as req
 import json
 import sys
+import os
 
 if len(sys.argv) <= 1:
     print("Too few arguments.")
@@ -8,6 +9,11 @@ if len(sys.argv) <= 1:
     exit()
 if len(sys.argv) == 2:
     md5 = "tmp_md5/" + sys.argv[1] + ".json"
+    if not os.path.exists(md5):
+        f = open("tmp_classtable/" + sys.argv[1] + ".txt", "w")
+        f.write("error")
+        f.close()
+        exit()
     userdata = open(md5, "r", encoding="utf-8")
     userdata = userdata.read()
     it = json.loads(userdata)
@@ -20,22 +26,29 @@ else:
     print("用户名是：" + username + "\n密码是：" + pwd)
 
 try:
+    if not os.path.exists("tmp_classtable/"):
+        os.mkdir("tmp_classtable/")
     """print(users[item]["sid"],users[item]["id"])"""
     data = {"userName": username, "userPwd": pwd}
     s = req.session()
     res = s.post("http://jwdep.dhu.edu.cn/dhu/login_wz.jsp", data, timeout=10)
-
-    print("成功登陆！")
-    """res = s.get("http://jwdep.dhu.edu.cn/dhu/student/modifyselfinfo.jsp")"""
-    """info = str(res.content,"gbk")"""
-    """s_class = re.search("<td>班级</td><td>(.*)</td>",info)"""
-    """s_class = s_class.groups()[0]"""
-    classtable = s.get("http://jwdep.dhu.edu.cn/dhu/student/selectcourse/seeselectedcourse.jsp", timeout=10).content
-    classtable = str(classtable, "gbk")
-    print("成功获取课表，正在储存...")
-    f = open("tmp_classtable/" + sys.argv[1] + ".txt", "w")
-    f.write(classtable)
-    f.close()
+    text = str(res.content, "gbk")
+    if "用户名和密码输入错误" in text:
+        f = open("tmp_classtable/" + sys.argv[1] + ".txt", "w")
+        f.write("pwd_error")
+        f.close()
+    else:
+        print("成功登陆！")
+        """res = s.get("http://jwdep.dhu.edu.cn/dhu/student/modifyselfinfo.jsp")"""
+        """info = str(res.content,"gbk")"""
+        """s_class = re.search("<td>班级</td><td>(.*)</td>",info)"""
+        """s_class = s_class.groups()[0]"""
+        classtable = s.get("http://jwdep.dhu.edu.cn/dhu/student/selectcourse/seeselectedcourse.jsp", timeout=10).content
+        classtable = str(classtable, "gbk")
+        print("成功获取课表，正在储存...")
+        f = open("tmp_classtable/" + sys.argv[1] + ".txt", "w")
+        f.write(classtable)
+        f.close()
 except KeyError:
     f = open("tmp_classtable/" + sys.argv[1] + ".txt", "w")
     f.write("error")
